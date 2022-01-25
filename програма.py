94b2e9f25d23b015d6ff47b40c2b5d4de9545225
ZRV <54231560+ZRVdeveloper@users.noreply.github.com>
2022-01-25 21:20:23 +0200

    Update програма.py

--- програма.py
+++ програма.py
@@ -2,13 +2,13 @@
 import tkinter as tk # Python 3
 root = tk.Tk()
 # The image must be stored to Tk or it will be garbage collected.
-root.image = tk.PhotoImage(file='1.png')
+root.image = tk.PhotoImage(file='1.png') #Назва файлк, що маэ бути поруч (можнв вказати будь-яку) - '1.png' 
 label = tk.Label(root, image=root.image, bg='white')
-#root.overrideredirect(True)#Ховає верзн
+#root.overrideredirect(True)#Ховає верхній рядок 
 root.geometry("+250+250")
 root.lift()
 root.wm_attributes("-topmost", True)
-#root.wm_attributes("-disabled", True) #Закріплює в 1 місці
+#root.wm_attributes("-disabled", True) #Закріплює в одному місці вікно на екрані
 root.wm_attributes("-transparentcolor", "white")
 label.pack()
 label.mainloop()
